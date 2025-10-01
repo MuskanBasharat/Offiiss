ca7c944674f06c2e2fa6bb9884abab3dbddccc97
Muskan <muskanbasharat1101@gmail.com>
2025-10-01 18:45:18 +0500

    commit

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -37,7 +37,9 @@ const Footer = () => {
                 <path d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160ZM176,24H80A56.06,56.06,0,0,0,24,80v96a56.06,56.06,0,0,0,56,56h96a56.06,56.06,0,0,0,56-56V80A56.06,56.06,0,0,0,176,24Zm40,152a40,40,0,0,1-40,40H80a40,40,0,0,1-40-40V80A40,40,0,0,1,80,40h96a40,40,0,0,1,40,40ZM192,76a12,12,0,1,1-12-12A12,12,0,0,1,192,76Z"/>
               </svg>
             </a>
+
           </div>
+          
         </div>
 
         {/* Navigation Links */}
@@ -53,19 +55,19 @@ const Footer = () => {
 
           {/* Contact Links */}
           <div className="link-group">
-            <div className="group-title">Phone:</div>
+            <div className="group-title">Contact:</div>
             <div className="link-item">US, Can: <a href="tel:+16046183396" className='link-hover'> +1 604-618-3396</a></div>
           <div className="link-item">Ghana: <a href="tel:+233572012642" className='link-hover'> +233 572-012-42</a></div>
+                      <div className="link-item">Email: <a href="mailto:Support@offiiss.com" className='link-hover'>Support@offiiss.com</a></div>
+                                             <div className="link-item">© 2025 Offiiss.</div>
+
+
           </div>
 
           {/* Legal Links */}
-          <div className="link-group">
-            <div className="group-title">Email:</div>
-            <div className="link-item"><a href="mailto:Support@offiiss.com" className='link-hover'>Support@offiiss.com</a></div>
-           <div className="link-item">© 2025 Offiiss.</div>
 
             
-          </div>
+          
         </div>
         
          <div className="social-icons hidden-sm">
